43b29cbbda4e7c2fe112bc19d0363fe2b0231eaf
Shelley Vohr <shelley.vohr@gmail.com>
2017-10-16 20:06:24 -0400

    ensure doc always has title

--- script/build.js
+++ script/build.js
@@ -62,7 +62,7 @@ async function parseFile (file) {
 
   // derive props from the HTML
   const $ = cheerio.load(file.html || '')
-  file.title = $('h1').text()
+  file.title = $('h1').text() !== '' ? $('h1').text() : $('h2').text() 
   file.description = $('blockquote').text()
 
   // remove leftover file props from walk-sync
