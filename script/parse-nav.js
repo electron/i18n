6605a067cddd864bf27176f1dc9012e05d35ba57
Shivam Miglani <shivam@Shivams-MacBook-Air.local>
2018-04-04 05:01:42 +0200

    Added parse-nav.js script
    - Parses `content/locale/docs/README.md` to get HTML of “Guides and
    Tutorials” (2nd heading) unordered list for each locale.
    - This is first step for adding sidebar nav to tutorial pages.
    - added script to package.json

---
+++ script/parse-nav.js
@@ -0,0 +1,29 @@
+const cheerio = require('cheerio')
+const path = require('path')
+const fs = require('fs')
+const i18n = require('..')
+const locales = Object.keys(i18n.locales)
+
+function getNav(locale) {
+  const docs = i18n.docs[locale]
+  const readme = docs['/docs/README']
+  const html = readme.sections.map(section => section.html).join('\n')
+  const $ = cheerio.load(html)
+  const startHeading = $('h2')[1]
+  const listItems = $(startHeading).next('ul').html()
+  const nav = `<ul>${listItems}</ul>`
+  return nav
+}
+
+const navsByLocale = locales
+    .reduce((acc, locale) => {
+      acc[locale] = getNav(locale)
+      return acc
+    }, {})
+
+i18n.navs = navsByLocale
+
+fs.writeFileSync(
+  path.join(__dirname, '../index.json'),
+  JSON.stringify(i18n, null, 2)
+)
