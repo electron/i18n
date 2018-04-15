283f9038ba6a94e3b490f71d5a4429ea55ceb9da
Zeke Sikelianos <zeke@github.com>
2018-04-15 10:35:39 -0700

    feat: wordcount (#306)
    * wordcount
    
    * tweak wordcount output
    
    * lint
    
    * increase test timeout
    
    * get wordcount for all languages

---
+++ script/wordcount.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const {chain} = require('lodash')
+const walk = require('walk-sync')
+const matchWords = require('match-words')
+
+console.log(`# Word Count`)
+
+analyze(path.join(__dirname, '../content/en-US'), 'English')
+analyze(path.join(__dirname, '../content'), 'All Languages')
+
+function analyze(dir, title) {
+  const files = walk(dir, {directories: false}).map(f => path.join(dir, f))
+  const words = chain(files)
+    .map(file => matchWords(fs.readFileSync(file, 'utf8')))
+    .flatten()
+    .value()
+  const counts = files.map(f => (matchWords(fs.readFileSync(f, 'utf8')) || []).length)
+  const {average, sum} = require('simple-statistics')
+  
+  const results = {
+    'total files': files.length,
+    'total words': sum(counts),
+    'unique words': chain(words).flatten().uniq().value().length,
+    'average words per file': Math.floor(average(counts))
+  }
+
+  console.log(`\n## ${title}\n`)
+  console.log('Stat | Value')
+  console.log('---- | -----')
+
+  Object.keys(results).forEach(stat => {
+    console.log([stat, results[stat]].join(' | '))
+  })
+}
---
+++ wordcount.md
@@ -0,0 +1,19 @@
+# Word Count
+
+## English
+
+Stat | Value
+---- | -----
+total files | 161
+total words | 155583
+unique words | 8034
+average words per file | 966
+
+## All Languages
+
+Stat | Value
+---- | -----
+total files | 3996
+total words | 3026896
+unique words | 70695
+average words per file | 757
