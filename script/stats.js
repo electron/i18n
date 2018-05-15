2b7edb553f41db19ea975696d9b7f86a7d9ac049
Zeke Sikelianos <zeke@sikelianos.com>
2018-05-15 14:18:23 -0700

    remove hebrew temporarily

--- script/stats.js
+++ script/stats.js
@@ -6,8 +6,12 @@ const path = require('path')
 const url = 'https://electronjs.org/crowdin/status'
 
 got(url, {json: true}).then(data => {
+  // TODO: re-enable when Hebrew bug is fixed: https://github.com/electron/i18n/issues/337
+  const stats = data.body
+    .filter(lang => lang.name !== 'Hebrew')
+
   fs.writeFileSync(
     path.join(__dirname, '../stats.json'),
-    JSON.stringify(data.body, null, 2)
+    JSON.stringify(stats, null, 2)
   )
 })
