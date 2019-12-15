b8be58717073a3a48bfa2e42470d919c0ae67261
HashimotoYT <hashimoto.stream@gmail.com>
2019-12-15 18:36:03 +0000

    build(deps): ugprade got to 10

--- script/stats.ts
+++ script/stats.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env ts-node
 
-import * as got from 'got'
+import got from 'got'
 import * as fs from 'fs'
 import * as path from 'path'
 const url = 'https://electronjs.org/crowdin/status'
 
-got(url, { json: true }).then(data => {
+got(url, { responseType: 'json' }).then(data => {
   fs.writeFileSync(
     path.join(__dirname, '../stats.json'),
     JSON.stringify(data.body, null, 2)
